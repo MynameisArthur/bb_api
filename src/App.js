cfc44b4f4567f9cf0e11c251424134787e8f2a5f
Arthur <arthurd800@wp.pl>
2020-07-04 12:41:47 +0200

    created Header component, connected with www.breakingbadapi.com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/ui/Header';
+
 
 const App = () => {
   return (
     <div className="container">
-      Hollay
+      <Header />
     </div>
   );
 };
