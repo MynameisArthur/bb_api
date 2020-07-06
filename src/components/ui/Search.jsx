632e45b5c340269a8b00f0fb0365a2cb6010201d
Arthur <arthurd800@wp.pl>
2020-07-06 08:06:28 +0200

    finished app

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('');
+    const onChange = (q) => {
+        setText(q);
+        getQuery(q);
+    };
     return (
         <section className="search">
             <form action="">
-                <input type="text" className="" />
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search characters"
+                    autoFocus
+                    value={text}
+                    onChange={e => onChange(e.target.value)}
+                />
             </form>
         </section>
     );
