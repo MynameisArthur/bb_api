79366ea5e9184dc6c5766f12c89e408d66fbfff2
Arthur <arthurd800@wp.pl>
2020-07-21 11:34:20 +0200

    small test commit

--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import CharacterItem from './CharacterItem';
 import Spinner from '../ui/Spinner';
 
-
-
-const CharacterGrid = ({ items, isLoading }) => {
-    return isLoading ? (<Spinner />) : (<section className="cards">
-        {
-            items.map(item => (
+const CharacterGrid = ({items, isLoading}) => {
+    console.log('ITEMS: ', items);
+    return isLoading ? (
+        <Spinner />
+    ) : (
+        <section className="cards">
+            {items.map((item) => (
                 <CharacterItem key={item.char_id} item={item} />
-            ))
-        }
-    </section>);
-
+            ))}
+        </section>
+    );
 };
 
 export default CharacterGrid;
